92b974d9deaeb310f77b972b604a35915d7a07e4
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2020-10-20 13:17:34 +0300

    fixed requirements.txt doc

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -31,9 +31,9 @@ class Show(db.Model):
     __tablename__ = 'Show'
     id = db.Column(db.Integer, primary_key=True)
     artist_id = db.Column(
-        db.Integer, db.ForeignKey('Artist.id'), nullable=True)
+        db.Integer, db.ForeignKey('Artist.id'), nullable=False)
     start_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
 
 
 class Venue(db.Model):
@@ -41,13 +41,13 @@ class Venue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     #area = db.Column(db.Integer, db.ForeignKey('Area.id'), nullable=True)
-    city = db.Column(db.String)
-    state = db.Column(db.String)
+    city = db.Column(db.String, nullable=True)
+    state = db.Column(db.String, nullable=True)
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String(120)))
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=True)
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
